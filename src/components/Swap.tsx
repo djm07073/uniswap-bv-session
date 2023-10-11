05933a9b13dd61ef54909c10febd7260e8e1b3bb
suha jin <89185836+djm07073@users.noreply.github.com>
2023-10-11 21:32:35 +0900

    util function

--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -6,7 +6,17 @@ const getAmountOut = (
   amountIn: bigint,
   reserveIn: bigint,
   reserveOut: bigint
-) => {};
+) => {
+  const numerator = 1000n * reserveIn * reserveOut;
+
+  const denominator = 1000n * reserveIn + 997n * amountIn;
+
+  return reserveOut - numerator / denominator;
+};
+const roundToSixDecimalPlaces = (numString: string) => {
+  const num = Number(numString);
+  return Math.round(num * 1e6) / 1e6;
+};
 export default function Swap() {
   const [isMatic, setIsMatic] = useState<boolean>(true);
   const { reserveIn, reserveOut } = useReserve(isMatic, USDC, WMATIC);
