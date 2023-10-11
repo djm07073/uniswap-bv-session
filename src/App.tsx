220fac97aa32af3d1dece8ffb77eaa753ac16cb6
suha jin <89185836+djm07073@users.noreply.github.com>
2023-10-11 21:00:05 +0900

    template

--- src/App.tsx
+++ src/App.tsx
@@ -26,32 +26,39 @@ createWeb3Modal({ wagmiConfig, projectId, chains });
 
 export default function App() {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      {/* 지갑연결하기 */}
-      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
-      {/* 과정 사이 가로선 */}
-      <h2 className="font-bold font-mono text-2xl"> 1. Wallet Connect</h2>
-      <w3m-button />
-      <MyUSDC />
-      {/* MATIC 양 입력 , 받게 될 USDC 계산됨*/}
-      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
-      {/* 과정 사이 가로선 */}
-      {/* 스왑연동하기 */}
-      <h2 className="font-bold font-mono text-2xl"> 2. Swap MATIC to USDC</h2>
-      <Swap />
-      {/* 유동성 공급/출금하기 */}
-      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
-      {/* 과정 사이 가로선 */}
-      <h2 className="font-bold font-mono text-2xl">
-        3. Add Liquidity on MATIC-USDC POOL
-      </h2>
-      {/* <AddLiquidity /> */}
-      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
-      {/* 과정 사이 가로선 */}
-      <h2 className="font-bold font-mono text-2xl">
-        4. Withdraw Liquidity on MATIC-USDC POOL
-      </h2>
-      {/* <WithdrawLiquidity /> */}
-    </WagmiConfig>
+    <div className="flex flex-col mt-10 items-center">
+      {" "}
+      {/* 가운데 정렬 */}
+      <h1 className="text-4xl font-mono">
+        BlockchainValley Session: Uniswap-V2 Clone
+      </h1>
+      <WagmiConfig config={wagmiConfig}>
+        {/* 지갑연결하기 */}
+        <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+        {/* 과정 사이 가로선 */}
+        <h2 className="font-bold font-mono text-2xl"> 1. Wallet Connect</h2>
+        <w3m-button />
+        <MyUSDC />
+        {/* MATIC 양 입력 , 받게 될 USDC 계산됨*/}
+        <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+        {/* 과정 사이 가로선 */}
+        {/* 스왑연동하기 */}
+        <h2 className="font-bold font-mono text-2xl"> 2. Swap MATIC to USDC</h2>
+        <Swap />
+        {/* 유동성 공급/출금하기 */}
+        <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+        {/* 과정 사이 가로선 */}
+        <h2 className="font-bold font-mono text-2xl">
+          3. Add Liquidity on MATIC-USDC POOL
+        </h2>
+        {/* <AddLiquidity /> */}
+        <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+        {/* 과정 사이 가로선 */}
+        <h2 className="font-bold font-mono text-2xl">
+          4. Withdraw Liquidity on MATIC-USDC POOL
+        </h2>
+        {/* <WithdrawLiquidity /> */}
+      </WagmiConfig>
+    </div>
   );
 }
