66a032a7642edc8ebd277cfbccab0b4266740ccd
suha jin <89185836+djm07073@users.noreply.github.com>
2023-10-11 21:30:15 +0900

    get Amount Out setting

--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import useReserve from "../hooks/useResrve";
 import { USDC, WMATIC } from "../App";
-
+import { ethers } from "ethers";
+const getAmountOut = (
+  amountIn: bigint,
+  reserveIn: bigint,
+  reserveOut: bigint
+) => {};
 export default function Swap() {
   const [isMatic, setIsMatic] = useState<boolean>(true);
   const { reserveIn, reserveOut } = useReserve(isMatic, USDC, WMATIC);
   return (
     <div>
-      <div>Reserve In: {reserveIn.toString()}</div>
-      <div>Reserve Out: {reserveOut.toString()}</div>
+      <div>Reserve In: {ethers.formatUnits(reserveIn, 18)}</div>
+      <div>Reserve Out: {ethers.formatUnits(reserveOut, 6)}</div>
     </div>
   );
 }
