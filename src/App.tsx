ad7cd146a51f1122f5da9d2cccb11509151235e7
suha jin <89185836+djm07073@users.noreply.github.com>
2023-10-11 20:52:40 +0900

    tempelate

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
 import { WagmiConfig } from "wagmi";
 import { polygon } from "wagmi/chains";
 import MyUSDC from "./components/MyUSDC";
+import Swap from "./components/Swap";
 export const ROUTER02 = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
 export const FACTORY = "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32";
 export const USDC = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
@@ -26,10 +27,31 @@ createWeb3Modal({ wagmiConfig, projectId, chains });
 export default function App() {
   return (
     <WagmiConfig config={wagmiConfig}>
-      {/* 지갑 연결하기 */}
+      {/* 지갑연결하기 */}
+      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+      {/* 과정 사이 가로선 */}
+      <h2 className="font-bold font-mono text-2xl"> 1. Wallet Connect</h2>
       <w3m-button />
-      {/* 나의 USDC */}
       <MyUSDC />
+      {/* MATIC 양 입력 , 받게 될 USDC 계산됨*/}
+      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+      {/* 과정 사이 가로선 */}
+      {/* 스왑연동하기 */}
+      <h2 className="font-bold font-mono text-2xl"> 2. Swap MATIC to USDC</h2>
+      <Swap />
+      {/* 유동성 공급/출금하기 */}
+      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+      {/* 과정 사이 가로선 */}
+      <h2 className="font-bold font-mono text-2xl">
+        3. Add Liquidity on MATIC-USDC POOL
+      </h2>
+      {/* <AddLiquidity /> */}
+      <div className="my-4 border-b-2 border-solid border-gray-500 w-1/2"></div>{" "}
+      {/* 과정 사이 가로선 */}
+      <h2 className="font-bold font-mono text-2xl">
+        4. Withdraw Liquidity on MATIC-USDC POOL
+      </h2>
+      {/* <WithdrawLiquidity /> */}
     </WagmiConfig>
   );
 }
